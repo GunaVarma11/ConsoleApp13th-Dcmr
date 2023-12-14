b534c5b6a4505aaf6aa8aa9ee1c247442c91cf88
gunav <gunav@VINESH>
2023-12-14 11:39:11 +0530

    Add project files.

---
+++ ConsoleApp13th Dcmr/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13th_Dcmr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whats up");
+            Console.ReadLine();
+        }
+    }
+}
